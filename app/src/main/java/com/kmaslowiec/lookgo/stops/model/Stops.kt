f536cc6b142c2eb4b9c1efbf2af6fe4f379a2adf
konrad <kmaslowiec@gmail.com>
2025-05-24 20:01:20 +0200

    3 connect to api

---
+++ app/src/main/java/com/kmaslowiec/lookgo/stops/model/Stops.kt
@@ -0,0 +1,5 @@
+package com.kmaslowiec.lookgo.stops.model
+
+data class Stops(
+    val data: List<Stop>
+)
---
+++ app/src/main/java/com/kmaslowiec/lookgo/stops/repository/StopRepository.kt
@@ -0,0 +1,21 @@
+package com.kmaslowiec.lookgo.stops.repository
+
+import android.util.Log
+import com.kmaslowiec.lookgo.api.ApiService
+import com.kmaslowiec.lookgo.stops.model.Stops
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class StopRepository @Inject constructor(private val api: ApiService) {
+
+    fun getStops(): Flow<Stops> = flow {
+        try {
+            emit(api.getStops())
+        } catch (e: Exception) {
+            Log.e("Connection error", "${e.message}")
+        }
+    }.flowOn(Dispatchers.IO)
+}
